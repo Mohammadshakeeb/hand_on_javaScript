8b0d230d7d9b3de36e42caccc246497bd3a25c22
Mohammad Shakeeb <mohammadshakeeb@cedcoss.com>
2022-02-05 13:30:44 +0530

    calling func

--- scr/script.js
+++ scr/script.js
@@ -208,5 +208,13 @@ function addFive(){
 
 addThree();
 addFive();
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed=processArg(7);
 
 
