56f26a2ce66ce02b764187c95158b6692adc23ac
Mohammad Shakeeb <mohammadshakeeb@cedcoss.com>
2022-02-05 14:53:34 +0530

    return bool val from fucn

--- scr/script.js
+++ scr/script.js
@@ -515,5 +515,13 @@ function nextInLine(arr, item) {
         // Only change code above this line
         return answer;
       }
+      function isLess(a, b) {
+        // Only change code below this line
+        return a<b;
+        
+        // Only change code above this line
+      }
+      
+      isLess(10, 15);
       
 
