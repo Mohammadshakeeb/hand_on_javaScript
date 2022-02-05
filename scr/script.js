49ebd8cfd2801d6d865a0ba62e589007f52b574d
Mohammad Shakeeb <mohammadshakeeb@cedcoss.com>
2022-02-05 13:50:08 +0530

    != op

--- scr/script.js
+++ scr/script.js
@@ -269,5 +269,13 @@ function nextInLine(arr, item) {
   }
   
   compareEquality(10, "10");
+  function testNotEqual(val) {
+    if (val!=99) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testNotEqual(10);
   
 
