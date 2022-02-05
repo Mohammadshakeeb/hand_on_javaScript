1ccc4ad49f7512a9fd26b51e03fb0d7083ff6a66
Mohammad Shakeeb <mohammadshakeeb@cedcoss.com>
2022-02-05 13:12:48 +0530

    functions add

--- scr/script.js
+++ scr/script.js
@@ -146,4 +146,10 @@ const myList = [
         console.log("Hi World")
       }
       reusableFunction();
-      
+      function functionWithArgs(p1, p2) {
+        console.log(p1+p2);
+      }
+      functionWithArgs(1,2);
+      functionWithArgs(7,9);
+
+
