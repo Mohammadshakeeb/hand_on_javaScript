bb92ef3e8b16185fed468e75c8d1bf2df98ef3dc
Mohammad Shakeeb <mohammadshakeeb@cedcoss.com>
2022-02-05 12:29:25 +0530

    .pop func

--- scr/script.js
+++ scr/script.js
@@ -122,4 +122,8 @@ const myArray = [
   const myData = myArray[2][1];
   const myArray = [["John", 23], ["cat", 2]];
 myArray.push(["dog",3]);
+const myArray = [["John", 23], ["cat", 2]];
 
+// Only change code below this line
+ const removedFromMyArray = myArray.pop();
+ 
