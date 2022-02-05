767906402881ceb785498f6fc978e0ea88722840
Mohammad Shakeeb <mohammadshakeeb@cedcoss.com>
2022-02-05 12:31:26 +0530

    .shift func

--- scr/script.js
+++ scr/script.js
@@ -126,4 +126,8 @@ const myArray = [["John", 23], ["cat", 2]];
 
 // Only change code below this line
  const removedFromMyArray = myArray.pop();
- 
+ const myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+const removedFromMyArray=myArray.shift();
+
