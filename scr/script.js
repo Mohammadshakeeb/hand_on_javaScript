7e0c80dd8314997797e4eda1afa8cd17a5af2146
Mohammad Shakeeb <mohammadshakeeb@cedcoss.com>
2022-02-05 14:46:20 +0530

    switch

--- scr/script.js
+++ scr/script.js
@@ -460,5 +460,24 @@ function nextInLine(arr, item) {
     }
     return answer;
   }
+
+  function switchOfStuff(val) {
+    var answer = "";
+    // Only change code below this line
   
+    switch (val) {
+      case "a":
+        answer = "apple";
+        break;
+      case "b":
+        answer = "bird";
+        break;
+      case "c":
+        answer = "cat";
+        break;
+      default:
+        answer = "stuff";
+    }
+
+    
 
