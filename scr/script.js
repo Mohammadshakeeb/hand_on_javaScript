c2d95916d2d511d5de60e26dffecaad63dd7a3c4
Mohammad Shakeeb <mohammadshakeeb@cedcoss.com>
2022-02-05 11:50:43 +0530

    concat strings

--- scr/script.js
+++ scr/script.js
@@ -67,3 +67,7 @@ const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
 const myStr = "This is the start."+" This is the end."; 
 let myStr="This is the first sentence. "
 myStr+= "This is the second sentence.";
+// Only change code below this line
+const myName = "Mohammad Shakeeb";
+const myStr = "My name is "+ myName+ "and I am well!";
+
