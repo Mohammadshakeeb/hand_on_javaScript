08774ea015b2bf58c409cc1f0a4af142756e912f
Mohammad Shakeeb <mohammadshakeeb@cedcoss.com>
2022-02-05 13:27:59 +0530

    undefined value

--- scr/script.js
+++ scr/script.js
@@ -193,5 +193,20 @@ function myOutfit() {
 }
 
 myOutfit();
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive(){
+  sum=sum+5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
 
 
