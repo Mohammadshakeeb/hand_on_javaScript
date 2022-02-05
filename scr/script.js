8a77d9d3b4bf40e86873cf7d675e5a354e496579
Mohammad Shakeeb <mohammadshakeeb@cedcoss.com>
2022-02-05 13:48:34 +0530

    strictly equal

--- scr/script.js
+++ scr/script.js
@@ -261,4 +261,13 @@ function nextInLine(arr, item) {
   }
   
   testStrict(10);
+  function compareEquality(a, b) {
+    if (a === b) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  compareEquality(10, "10");
+  
 
