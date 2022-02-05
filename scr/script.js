8288fb6e4ce2566fe0956886b9da4fe6ae52477d
Mohammad Shakeeb <mohammadshakeeb@cedcoss.com>
2022-02-05 13:42:22 +0530

    if statement

--- scr/script.js
+++ scr/script.js
@@ -238,4 +238,11 @@ function nextInLine(arr, item) {
   
     // Only change code above this line
   }
+  function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+  if (wasThatTrue) {
+      return "Yes, that was true";
+    }
+    return "No, that was false";
+  }
 
