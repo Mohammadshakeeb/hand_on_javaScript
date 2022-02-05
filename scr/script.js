9b1ae71fcbd98fd6e6c96c87e02751d1b186a4a3
Mohammad Shakeeb <mohammadshakeeb@cedcoss.com>
2022-02-05 14:12:43 +0530

    || op

--- scr/script.js
+++ scr/script.js
@@ -351,5 +351,16 @@ function nextInLine(arr, item) {
   }
   
   testLogicalAnd(10);
+  function testLogicalOr(val) {
+    // Only change code below this line
+  
+    if (val<10 ||val>20) {
+      return "Outside";
+    }
+    // Only change code above this line
+    return "Inside";
+  }
+  
+  testLogicalOr(15);
   
 
